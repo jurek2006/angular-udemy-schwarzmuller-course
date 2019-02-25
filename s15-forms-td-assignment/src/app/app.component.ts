d39757dbd0ec82bffe1277076bb546bb759b0717
Jurek <jurek2006@gmail.com>
2019-02-25 15:58:31 +0100

    Sekcja 15 - moje rozwiązanie zadania template driven forms z modyfikacją

--- s15-forms-td-assignment/src/app/app.component.ts
+++ s15-forms-td-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') form: NgForm;
+  subscriptions = [
+    { label: 'Basic', value: 'basic' },
+    { label: 'Advanced', value: 'advanced' },
+    { label: 'Pro', value: 'pro', default: true }
+  ];
+  // defaultSubscription = this.subscriptions.find(
+  //   subscription => subscription.default === true
+  // );
 
+  submitted = false;
+  userSubscription = {
+    email: '',
+    password: '',
+    subscription: ''
+  };
+
+  defaultSubscription() {
+    return this.subscriptions.find(
+      subscription => subscription.default === true
+    ).value;
+  }
+  onSubmit() {
+    const { email, password, subscription } = this.form.value;
+    this.userSubscription = { email, password, subscription };
+    this.submitted = true;
+    this.form.reset({
+      subscription: this.defaultSubscription()
+    });
+    console.log(this.defaultSubscription);
+  }
 }
