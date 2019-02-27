b35e253147ac0b328b72614512f6929a0ed36b7e
Jurek <jurek2006@gmail.com>
2019-02-27 14:21:28 +0100

    Sekcja 15 - moje rozwiązanie zadania reactive forms

--- s15-forms-reactive-assignment/src/app/app.component.ts
+++ s15-forms-reactive-assignment/src/app/app.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  statuses = ['Stable', 'Critical', 'Finished'];
+  forbiddenProjectNames = ['Test'];
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(
+        null,
+        [Validators.required],
+        this.forbiddenNamesAsync
+      ),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      status: new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+    this.projectForm.reset();
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenProjectNames.includes(control.value)) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+  }
+
+  forbiddenNamesAsync = (
+    control: FormControl
+  ): Promise<any> | Observable<any> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (this.forbiddenProjectNames.includes(control.value)) {
+          resolve({ nameIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
+  };
 }
--- s15-forms-reactive-assignment/src/app/app.module.ts
+++ s15-forms-reactive-assignment/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
