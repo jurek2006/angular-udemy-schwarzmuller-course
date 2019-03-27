4c5a055c3c3819f5ffc2492eec93c46013e4aacb
Jurek <jurek2006@gmail.com>
2019-03-27 13:51:55 +0100

    311. Modifying Requests in Interceptors (Modifying response)

---
+++ s23-httpclient/src/app/shared/logging.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/do";
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).do(event => {
+      console.log("Logging interceptor:", event);
+    });
+  }
+}
